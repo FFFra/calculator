e61054d6e2c9b1acf6c1d5bb7aa3694b7563ce1e
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-10-30 16:46:28 +0000

    refactor(app.js): turns App.js into stateful component

--- App.js
+++ App.js
@@ -6,38 +6,40 @@
  * @flow
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text, StatusBar, SafeAreaView, StyleSheet} from 'react-native';
 import Button from './src/components/Button';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        <View style={styles.button}>
-          <Button label="AC" />
-          <Button label="/" />
-          <Button label="7" />
-          <Button label="8" />
-          <Button label="9" />
-          <Button label="*" />
-          <Button label="4" />
-          <Button label="5" />
-          <Button label="6" />
-          <Button label="-" />
-          <Button label="1" />
-          <Button label="2" />
-          <Button label="3" />
-          <Button label="+" />
-          <Button label="0" />
-          <Button label="." />
-          <Button label="=" />
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.container}>
+          <View style={styles.button}>
+            <Button label="AC" />
+            <Button label="/" />
+            <Button label="7" />
+            <Button label="8" />
+            <Button label="9" />
+            <Button label="*" />
+            <Button label="4" />
+            <Button label="5" />
+            <Button label="6" />
+            <Button label="-" />
+            <Button label="1" />
+            <Button label="2" />
+            <Button label="3" />
+            <Button label="+" />
+            <Button label="0" />
+            <Button label="." />
+            <Button label="=" />
+          </View>
         </View>
-      </View>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -48,5 +50,3 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
 });
-
-export default App;
